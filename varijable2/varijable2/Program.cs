e8944082146615e2dbee0d37e124559e9b8494e2
LukaTepsic <166146790+LukaTepsic@users.noreply.github.com>
2024-10-13 13:28:20 +0200

    Add files via upload

---
+++ varijable2/varijable2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Unesite jedan cijeli i jedan decimalni broj odvojene razmakom:");
+        string input = Console.ReadLine();
+        string[] numbers = input.Split(' ');
+
+        int x = int.Parse(numbers[0]);
+        double y = double.Parse(numbers[1]);
+
+        x *= 2;
+        y *= 2;
+
+        Console.WriteLine($"Dvostruka vrijednost cijelog broja: {x}");
+        Console.WriteLine($"Dvostruka vrijednost decimalnog broja: {y}");
+    }
+}
+
+
